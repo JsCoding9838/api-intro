bfb3d193bf4bcf87b0fff02adccf5e5a4ddd975f
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-02-22 01:29:51 +0800

    first commit

---
+++ js/api.js
@@ -0,0 +1,31 @@
+function setData(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(respons => respons.json())
+    .then(data => console.log(data));
+};
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUsers(data));
+};
+
+function loadPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => console.log(data));
+};
+function displayUsers(data){
+    const ol = document.getElementById('users');
+    for(const user of data){
+        // console.log(user.company.name);
+        const li = document.createElement('li');
+        li.innerText = `
+            Name: ${user.name}
+            Email: ${user.email}
+            Company: ${user.company.name}
+        `;
+
+        ol.appendChild(li);
+    }
+};
